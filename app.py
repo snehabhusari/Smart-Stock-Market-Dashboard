8f83f16c7eb3b4ef61196de0346d862553ade808
snehabhusari <142480400+snehabhusari@users.noreply.github.com>
2026-06-21 16:18:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+from flask import Flask, request, jsonify
+from ticker import fetch_ticker
 from main import launch_ui
 import os
 
+app = Flask(__name__)
+
+@app.route('/api/getTicker', methods=['GET'])
+def get_ticker():
+    company = request.args.get('company')
+    symbol = fetch_ticker(company)
+    return jsonify({"company": company, "symbol": symbol})
+
 if __name__ == "__main__":
-   
     port = int(os.environ.get("PORT", 10000))
     launch_ui().launch(server_name="0.0.0.0", server_port=port)
